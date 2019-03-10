2846a855dd7b69502410a6fc5cabc9a03ef231e4
ny3an6 <fytyrka@mail.ru>
2019-03-10 23:29:44 +0300

    time_is_solve

--- selenium_3.py
+++ selenium_3.py
@@ -3,6 +3,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 # https://www.hostinger.ru/rukovodstva/osnovnie-git-komandy
 # http://python-3.ru/page/selenium-python-example
 class PythonOrgSearch(unittest.TestCase):
@@ -12,22 +15,26 @@ class PythonOrgSearch(unittest.TestCase):
 
     def test_search_in_python_org(self):
         driver = self.driver # (1)
-        driver.get("http://192.168.5.119/")
+        driver.get("http://192.168.5.118/")
         login = driver.find_element_by_name("login")
         login.send_keys("roma")
         password = driver.find_element_by_name("password")
         password.send_keys("roma")
         button = driver.find_element_by_class_name("ant-btn-primary")
         button.submit()
-        time.sleep(4)      
-        #status = driver.find_element_by_class_name("ant-table-row-level-0")      
-        #action = ActionChains(driver)
-        #action.move_to_element(status).click().perform()
-        #time.sleep(3)
-        #hold_time = driver.find_elements_by_class_name("ant-col-12")
-        #for times in hold_time:
-        #    print("<p>: " % times.get_attribute("p"))
 
+        driver.wait = WebDriverWait(driver, 10)
+        inf_call = driver.wait.until(EC.presence_of_element_located((By.CLASS_NAME, "ant-table-row-level-0")))     
+        action = ActionChains(driver)
+        action.move_to_element(inf_call).click().perform()
+
+        try:
+            hold_time_all = driver.wait.untill(EC.presence_of_element_located((
+                By.CLASS_NAME, "p-font")))
+            asd = hold_time_all.text.split()
+            print(asd[-1])
+        except:
+            print("Error") 
 
 
    # def tearDown(self):
