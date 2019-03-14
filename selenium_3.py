f5937af691572a7fca1563b702947bd0bddf675b
ny3an6 <fytyrka@mail.ru>
2019-03-14 11:48:24 +0300

    test

--- selenium_3.py
+++ selenium_3.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,17 +11,17 @@ from selenium.webdriver.support import expected_conditions as EC
 # http://python-3.ru/page/selenium-python-example
 class PythonOrgSearch(unittest.TestCase):
 
-    def setUp(self): # зачем отдельно(1)
+    def setUp(self): 
         self.driver = webdriver.Firefox()
 
     def test_search_in_python_org(self):
-        driver = self.driver # (1)
-        driver.get("http://192.168.5.119/")
+        driver = self.driver 
+        driver.get("http://192.168.5.118/")
         assert "React App" in driver.title
         login = driver.find_element_by_name("login")
-        login.send_keys("xxxx")
+        login.send_keys("roma")
         password = driver.find_element_by_name("password")
-        password.send_keys("xxxx")
+        password.send_keys("roma")
         button = driver.find_element_by_class_name("ant-btn-primary")
         button.submit()
 
@@ -28,18 +29,23 @@ class PythonOrgSearch(unittest.TestCase):
         inf_call = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "ant-table-row-level-0")))     
         action = ActionChains(driver)
         action.move_to_element(inf_call).click().perform()
-     #   time.sleep(2)
+     
         try:
-             wait_2 = wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "p-font")))      
+             wait_2 = wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "p-font"))) 
+             #wait_2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(), 'YourTextHere')]")));     
         except:
             print("Error") 
-        else:
-           # hold_time_all = wait.until(EC.presence_of_element_located((
-           #     By.CLASS_NAME, "p-font")))
+        else:  
             asd = wait_2[-1].text.split()
-            print(asd[-1])
-            if int(asd[-1]) <= 0:
-                print("Error, value of waiting time is equally 0 or below")
+            print(asd)
+            os.environ["value_of_waiting_call"] = asd[-1]
+            try: 
+                if asd[-1] or asd[-3] or int(asd[-4]) or int(asd[-5]) <= 0:
+                    os.environ["val"] = "Error, value of waiting time is equally 0 or below"
+                    #print("Error, value of waiting time is equally 0 or below")
+                else:
+                    print[]
+
         finally:
             driver.close()
    # def tearDown(self):
