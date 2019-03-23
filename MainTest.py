16e7bf0d93fe6dd63aa79723ac7a1bfcfbf77152
ny3an6 <fytyrka@mail.ru>
2019-03-24 00:25:23 +0300

    logs

--- MainTest.py
+++ MainTest.py
@@ -2,6 +2,7 @@ import unittest
 import time
 import os
 import logging
+from logging import log
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -12,8 +13,8 @@ from selenium.webdriver.support import expected_conditions as EC
 logging.basicConfig(
     filename="logi.log", 
     level=logging.DEBUG,
-    format="%(asctime)s:%(levelname)s:%(message)s"
-    )
+    format="%(asctime)s:%(levelname)s:%(message)s")
+    
 
 class PythonOrgSearch(unittest.TestCase):
 
@@ -28,9 +29,11 @@ class PythonOrgSearch(unittest.TestCase):
         password = driver.find_element_by_name("password").send_keys("roma")
         workplace = driver.find_element_by_name("phone").send_keys("2214")
         button = driver.find_element_by_class_name("ant-btn-primary").submit()
-        
+        driver = self.driver 
         action = ActionChains(driver)
         wait = WebDriverWait(driver, 10)
+    
+    
         
         history = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "anticon-bars")))
         action.move_to_element(history).click().perform()
@@ -49,26 +52,52 @@ class PythonOrgSearch(unittest.TestCase):
 
         table_vision = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "ant-table-row-level-0")))
         if table_vision:
-            logging.debug("Success")
+            logging.debug("Success, history of calling is visible")
         elif not table_vision:
-            logging.debug("Error, no table elements")
+            logging.debug("Error, history of calling is invisible")
+
+        driver.close()
+        
+    def test_Case_2(self):
+        driver = self.driver 
+        driver.get("http://192.168.5.118/")
+        assert "React App" in driver.title
+        login = driver.find_element_by_name("login").send_keys("roma")
+        password = driver.find_element_by_name("password").send_keys("roma")
+        workplace = driver.find_element_by_name("phone").send_keys("2214")
+        button = driver.find_element_by_class_name("ant-btn-primary").submit()
+        driver = self.driver 
+        action = ActionChains(driver)
+        wait = WebDriverWait(driver, 10)
 
 
         graphics = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "anticon-bar-chart")))
         action.move_to_element(graphics).click().perform()
         
-        time.sleep(2)
-        # date_3 = wait.until(EC.visibility_of_element_located((By.ID, "DateFrom"))).find_element_by_tag_name("input")
-        date_3 = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@id='DateFrom']/div/input[1]")))
-        print(date_3)
+        
+        date_3 = wait.until(EC.visibility_of_element_located((By.ID, "DateFrom"))).find_element_by_tag_name("input")
+        #date_3 = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@id='DateFrom']/div/input[1]")))
+        action.move_to_element(date_3).click().send_keys("2019-03-03").perform()
+        
+        action.move_to_element(graphics).click().perform()
 
-        action.move_to_element(date_3).click().send_keys("2019-03-21").perform()
+        date_4 = wait.until(EC.visibility_of_element_located((By.ID, "DateTo"))).find_element_by_tag_name("input")
+        action.move_to_element(date_4).click().send_keys("2019-03-21").perform()
 
+        action.move_to_element(graphics).click().perform()
+
+        graphics_button = driver.find_element_by_class_name("ant-btn")
+        action.move_to_element(graphics_button).click().perform()
+
+        #first_data_graphic = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@id='DateFrom']/div/input[1]")))
+        first_data_graphic = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "recharts-cartesian-axis-tick")))
+        if first_data_graphic:
+            logging.debug("Success, graphics is visible")
+        elif not first_data_graphic:
+            logging.debug("Error, graphics is invisible")
         
 
-
-
-        time.sleep(10)
+        
        
 if __name__ == "__main__":
     unittest.main()
